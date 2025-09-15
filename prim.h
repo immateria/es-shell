d2df0d215c0f6870637a7e19a0df1802edb25ff8
Thomas Soteros <thomas.soteros@gmail.com>
2025-09-15 00:02:46 -0500

    Refactor extern declarations in prim.h

--- prim.h
+++ prim.h
@@ -14,8 +14,9 @@ typedef struct { List *(*prim)(List *, Binding *, int); } Prim;
 			))
 
 extern Dict *initprims_controlflow(Dict *primdict);	/* prim-ctl.c */
-extern Dict *initprims_io(Dict *primdict);		/* prim-io.c */
-extern Dict *initprims_etc(Dict *primdict);		/* prim-etc.c */
-extern Dict *initprims_sys(Dict *primdict);		/* prim-sys.c */
-extern Dict *initprims_proc(Dict *primdict);		/* proc.c */
-extern Dict *initprims_access(Dict *primdict);		/* access.c */
+extern Dict *initprims_io(         Dict *primdict); /* prim-io.c */
+extern Dict *initprims_etc(        Dict *primdict);	/* prim-etc.c */
+extern Dict *initprims_math(       Dict *primdict);	/* prim-math.c */
+extern Dict *initprims_sys(        Dict *primdict);	/* prim-sys.c */
+extern Dict *initprims_proc(       Dict *primdict);	/* proc.c */
+extern Dict *initprims_access(     Dict *primdict);	/* access.c */
