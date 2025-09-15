eda30b7248d99f1e3f873f8bc57567ab4b170cdd
Thomas Soteros <thomas.soteros@gmail.com>
2025-09-15 01:49:30 -0500

    Refactor vector functions with better naming and comments

--- vec.c
+++ vec.c
@@ -5,56 +5,85 @@
 
 DefineTag(Vector, static);
 
-extern Vector *mkvector(int n) {
-	int i;
-	Vector *v = gcalloc(offsetof(Vector, vector[n + 1]), &VectorTag);
-	v->alloclen = n;
-	v->count = 0;
-	for (i = 0; i <= n; i++)
-		v->vector[i] = NULL;
-	return v;
+/* mkvector -- create a new vector with specified capacity
+ * Arguments:
+ *   capacity: maximum number of elements the vector can hold
+ * Returns: newly allocated vector with count=0 and all elements set to NULL
+ */
+extern Vector *mkvector(int capacity)
+{   int vector_index;
+    Vector *new_vector = gcalloc(offsetof(Vector, vector[capacity + 1]), &VectorTag);
+    
+    new_vector->alloclen = capacity;
+    new_vector->count    = 0;
+    
+    /* Initialize all slots to NULL (including sentinel) */
+    for (vector_index = 0; vector_index <= capacity; vector_index++)
+        new_vector->vector[vector_index] = NULL;
+        
+    return new_vector;
 }
 
-static void *VectorCopy(void *ov) {
-	size_t n = offsetof(Vector, vector[((Vector *) ov)->alloclen + 1]);
-	void *nv = gcalloc(n, &VectorTag);
-	memcpy(nv, ov, n);
-	return nv;
+static void *VectorCopy(void *original_vector)
+{   size_t  allocation_size = offsetof(Vector, vector[((Vector *)original_vector)->alloclen + 1]);
+    void   *new_vector      = gcalloc(allocation_size, &VectorTag);
+    
+    memcpy(new_vector, original_vector, allocation_size);
+    return new_vector;
 }
 
-static size_t VectorScan(void *p) {
-	Vector *v = p;
-	int i, n = v->count;
-	for (i = 0; i <= n; i++)
-		v->vector[i] = forward(v->vector[i]);
-	return offsetof(Vector, vector[v->alloclen + 1]);
+static size_t VectorScan(void *vector_ptr)
+{   Vector *vector = vector_ptr;
+    int element_index;
+    int element_count = vector->count;
+    
+    /* Forward all pointers including the NULL sentinel */
+    for (element_index = 0; element_index <= element_count; element_index++)
+        vector->vector[element_index] = forward(vector->vector[element_index]);
+        
+    return offsetof(Vector, vector[vector->alloclen + 1]);
 }
 
+/* vectorize -- convert a list to a vector of strings
+ * Arguments:
+ *   list: linked list to convert
+ * Returns: vector containing string representations of all list elements
+ *          The vector will have a NULL sentinel at the end for use with execv() etc.
+ */
+extern Vector *vectorize(List *list)
+{   int element_index;
+    int list_length = length(list);
 
-extern Vector *vectorize(List *list) {
-	int i, n = length(list);
+    Ref(Vector *, result_vector, NULL);
+    Ref(List   *, current_list,  list);
+    
+    result_vector        = mkvector(list_length);
+    result_vector->count = list_length;
 
-	Ref(Vector *, v, NULL);
-	Ref(List *, lp, list);
-	v = mkvector(n);
-	v->count = n;
+    /* Copy string values from list to vector */
+    for (element_index = 0; current_list != NULL; current_list = current_list->next, element_index++)
+    {   char *string_value                   = getstr(current_list->term); /* must evaluate before assignment */
+        result_vector->vector[element_index] = string_value;
+    }
 
-	for (i = 0; lp != NULL; lp = lp->next, i++) {
-		char *s = getstr(lp->term); /* must evaluate before v->vector[i] */
-		v->vector[i] = s;
-	}
-
-	RefEnd(lp);
-	RefReturn(v);
+    RefEnd(current_list);
+    RefReturn(result_vector);
 }
 
-/* qstrcmp -- a strcmp wrapper for qsort */
-extern int qstrcmp(const void *s1, const void *s2) {
-	return strcmp(*(const char **)s1, *(const char **)s2);
+/* qstrcmp -- string comparison wrapper for qsort()
+ * Arguments:
+ *   string1, string2: pointers to char* pointers (as required by qsort)
+ * Returns: negative, zero, or positive value for string comparison
+ */
+extern int qstrcmp(const void *string1, const void *string2)
+{   return strcmp(*(const char **)string1, *(const char **)string2);
 }
 
-/* sortvector */
-extern void sortvector(Vector *v) {
-	assert(v->vector[v->count] == NULL);
-	qsort(v->vector, v->count, sizeof (char *), qstrcmp);
+/* sortvector -- sort vector elements alphabetically in place
+ * Arguments:
+ *   vector: vector to sort (must have NULL sentinel at vector[count])
+ */
+extern void sortvector(Vector *vector)
+{   assert(vector->vector[vector->count] == NULL);
+    qsort(vector->vector, vector->count, sizeof(char *), qstrcmp);
 }
