5813a0a8827d869af5f1a81abd5673c76ee53854
Thomas Soteros <thomas.soteros@gmail.com>
2025-09-15 00:56:28 -0500

    Refactor closure.c for improved readability

--- closure.c
+++ closure.c
@@ -9,145 +9,165 @@
 
 DefineTag(Closure, static);
 
-extern Closure *mkclosure(Tree *tree, Binding *binding) {
-	gcdisable();
-	Ref(Closure *, closure, gcnew(Closure));
-	closure->tree = tree;
-	closure->binding = binding;
-	gcenable();
-	RefReturn(closure);
+extern Closure *mkclosure(Tree *tree, Binding *binding)
+{   gcdisable();
+    Ref(Closure *, closure, gcnew(Closure));
+    closure->tree    = tree;
+    closure->binding = binding;
+    gcenable();
+    RefReturn(closure);
 }
 
-static void *ClosureCopy(void *op) {
-	void *np = gcnew(Closure);
-	memcpy(np, op, sizeof (Closure));
-	return np;
+static void *ClosureCopy(void *original_ptr)
+{   void *new_ptr = gcnew(Closure);
+    memcpy(new_ptr, original_ptr, sizeof(Closure));
+    return new_ptr;
 }
 
-static size_t ClosureScan(void *p) {
-	Closure *closure = p;
-	closure->tree = forward(closure->tree);
-	closure->binding = forward(closure->binding);
-	return sizeof (Closure);
+static size_t ClosureScan(void *closure_ptr)
+{   Closure *closure = closure_ptr;
+    closure->tree    = forward(closure->tree);
+    closure->binding = forward(closure->binding);
+    return sizeof(Closure);
 }
 
 /* revtree -- destructively reverse a list stored in a tree */
-static Tree *revtree(Tree *tree) {
-	Tree *prev, *next;
-	if (tree == NULL)
-		return NULL;
-	prev = NULL;
-	do {
-		assert(tree->kind == nList);
-		next = tree->u[1].p;
-		tree->u[1].p = prev;
-		prev = tree;
-	} while ((tree = next) != NULL);
-	return prev;
+static Tree *revtree(Tree *tree)
+{   Tree *previous_node;
+    Tree *next_node;
+    
+    if (tree == NULL)
+        return NULL;
+        
+    previous_node = NULL;
+    do
+    {   assert(tree->kind == nList);
+        next_node     = tree->u[1].p;
+        tree->u[1].p  = previous_node;
+        previous_node = tree;
+	 
+    }
+	while ((tree = next_node) != NULL);
+    
+    return previous_node;
 }
 
 typedef struct Chain Chain;
-struct Chain {
-	Closure *closure;
-	Chain *next;
+struct Chain
+{   Closure *closure;
+    Chain   *next;
 };
 static Chain *chain = NULL;
 
-static Binding *extract(Tree *tree, Binding *bindings) {
-	assert(gcisblocked());
+static Binding *extract(Tree *tree, Binding *bindings)
+{   assert(gcisblocked());
 
-	for (; tree != NULL; tree = tree->u[1].p) {
-		Tree *defn = tree->u[0].p;
-		assert(tree->kind == nList);
-		if (defn != NULL) {
-			List *list = NULL;
-			Tree *name = defn->u[0].p;
-			assert(name->kind == nWord || name->kind == nQword);
-			defn = revtree(defn->u[1].p);
-			for (; defn != NULL; defn = defn->u[1].p) {
-				Term *term;
-				Tree *word = defn->u[0].p;
-				NodeKind k = word->kind;
-				assert(defn->kind == nList);
-				assert(k == nWord || k == nQword || k == nPrim);
-				if (k == nPrim) {
-					char *prim = word->u[0].s;
-					if (streq(prim, "nestedbinding")) {
-						int i, count;
-						Chain *cp;
-						if (
-							(defn = defn->u[1].p) == NULL
-						     || defn->u[0].p->kind != nWord
-						     || (count = (atoi(defn->u[0].p->u[0].s))) < 0
-						) {
-							fail("$&parse", "improper use of $&nestedbinding");
-							NOTREACHED;
-						}
-						for (cp = chain, i = 0;; cp = cp->next, i++) {
-							if (cp == NULL) {
-								fail("$&parse", "bad count in $&nestedbinding: %d", count);
-								NOTREACHED;
-							}
-							if (i == count)
-								break;
-						}
-						term = mkterm(NULL, cp->closure);
-					} else {
-						fail("$&parse", "bad unquoted primitive in %%closure: $&%s", prim);
-						NOTREACHED;
-					}
-				} else
-					term = mkstr(word->u[0].s);
-				list = mklist(term, list);
-			}
-			bindings = mkbinding(name->u[0].s, list, bindings);
-		}
-	}
+    for (; tree != NULL; tree = tree->u[1].p)
+    {   Tree *definition = tree->u[0].p;
+        assert(tree->kind == nList);
+        
+        if (definition != NULL)
+        {   List *value_list    = NULL;
+            Tree *variable_name = definition->u[0].p;
+            
+            assert(variable_name->kind == nWord || variable_name->kind == nQword);
+            definition = revtree(definition->u[1].p);
+            
+            for (; definition != NULL; definition = definition->u[1].p)
+            {   Term     *term;
+                Tree     *word      = definition->u[0].p;
+                NodeKind  word_kind = word->kind;
+                
+                assert(definition->kind == nList);
+                assert(word_kind == nWord || word_kind == nQword || word_kind == nPrim);
+                
+                if (word_kind == nPrim)
+                {   char *primitive_name = word->u[0].s;
+                    
+                    if (streq(primitive_name, "nestedbinding"))
+                    {   int    chain_index;
+                        int    chain_count;
+                        Chain *chain_ptr;
+                        
+                        if ((definition = definition->u[1].p) == NULL
+                         || definition->u[0].p->kind != nWord
+                         || (chain_count = (atoi(definition->u[0].p->u[0].s))) < 0)
+                        {   fail("$&parse", "improper use of $&nestedbinding");
+                            NOTREACHED;
+                        }
+                        
+                        for (chain_ptr = chain, chain_index = 0;; chain_ptr = chain_ptr->next, chain_index++)
+                        {   if (chain_ptr == NULL)
+                            {   fail("$&parse", "bad count in $&nestedbinding: %d", chain_count);
+                                NOTREACHED;
+                            }
+						 
+                            if (chain_index == chain_count)
+                                break;
+                        }
+                        term = mkterm(NULL, chain_ptr->closure);
+                    }
+						
+                    else
+                    {   fail("$&parse", "bad unquoted primitive in %%closure: $&%s", primitive_name);
+                        NOTREACHED;
+                    }
+                }
+					
+                else
+                    term = mkstr(word->u[0].s);
+                    
+                value_list = mklist(term, value_list);
+            }
+            bindings = mkbinding(variable_name->u[0].s, value_list, bindings);
+        }
+    }
 
-	return bindings;
+    return bindings;
 }
 
-extern Closure *extractbindings(Tree *tree0) {
-	Chain me;
-	Tree *volatile tree = tree0;
-	Binding *volatile bindings = NULL;
+extern Closure *extractbindings(Tree *input_tree)
+{   Chain    current_chain_entry;
+    Tree    *volatile tree     = input_tree;
+    Binding *volatile bindings = NULL;
 
-	gcdisable();
+    gcdisable();
 
-	if (tree->kind == nList && tree->u[1].p == NULL)
-		tree = tree->u[0].p;
+    if (tree->kind == nList && tree->u[1].p == NULL)
+        tree = tree->u[0].p;
 
-	me.closure = mkclosure(NULL, NULL);
-	me.next = chain;
-	chain = &me;
+    current_chain_entry.closure = mkclosure(NULL, NULL);
+    current_chain_entry.next    = chain;
+    chain                       = &current_chain_entry;
 
-	ExceptionHandler
+    ExceptionHandler
 
-		while (tree->kind == nClosure) {
-			bindings = extract(tree->u[0].p, bindings);
-			tree = tree->u[1].p;
-			if (tree == NULL)
-				fail("$&parse", "null body in %%closure");
-			if (tree->kind == nList && tree->u[1].p == NULL)
-				tree = tree->u[0].p;
-		}
+        while (tree->kind == nClosure)
+        {   bindings = extract(tree->u[0].p, bindings);
+            tree     = tree->u[1].p;
+		 
+            if (tree == NULL)
+                fail("$&parse", "null body in %%closure");
+		 
+            if (tree->kind == nList && tree->u[1].p == NULL)
+                tree = tree->u[0].p;
+        }
 
-	CatchException (e)
+    CatchException(exception)
 
-		chain = chain->next;
-		throw(e);
+        chain = chain->next;
+        throw(exception);
 
-	EndExceptionHandler
+    EndExceptionHandler
 
-	chain = chain->next;
+    chain = chain->next;
 
-	Ref(Closure *, result, me.closure);
-	result->tree = tree;
-	result->binding = bindings;
-	gcenable();
-	RefReturn(result);
+    Ref(Closure *, result, current_chain_entry.closure);
+    result->tree    = tree;
+    result->binding = bindings;
+    gcenable();
+    RefReturn(result);
 }
-
 
 /*
  * Binding garbage collection support
@@ -155,43 +175,50 @@ extern Closure *extractbindings(Tree *tree0) {
 
 DefineTag(Binding, static);
 
-extern Binding *mkbinding(char *name, List *defn, Binding *next) {
-	assert(next == NULL || next->name != NULL);
-	validatevar(name);
-	gcdisable();
-	Ref(Binding *, binding, gcnew(Binding));
-	binding->name = name;
-	binding->defn = defn;
-	binding->next = next;
-	gcenable();
-	RefReturn(binding);
+extern Binding *mkbinding(char *name, List *definition, Binding *next)
+{   assert(next == NULL || next->name != NULL);
+    validatevar(name);
+    gcdisable();
+    Ref(Binding *, binding, gcnew(Binding));
+    binding->name = name;
+    binding->defn = definition;
+    binding->next = next;
+    gcenable();
+    RefReturn(binding);
 }
 
-extern Binding *reversebindings(Binding *binding) {
-	if (binding == NULL)
-		return NULL;
-	else {
-		Binding *prev, *next;
-		prev = NULL;
-		do {
-			next = binding->next;
-			binding->next = prev;
-			prev = binding;
-		} while ((binding = next) != NULL);
-		return prev;
-	}
+extern Binding *reversebindings(Binding *binding)
+{   if (binding == NULL)
+        return NULL;
+	
+    else
+    {   Binding *previous_binding;
+        Binding *next_binding;
+        
+        previous_binding = NULL;
+	 
+        do
+        {   next_binding     = binding->next;
+            binding->next    = previous_binding;
+            previous_binding = binding;
+        }
+		while ((binding = next_binding) != NULL);
+        
+        return previous_binding;
+    }
 }
 
-static void *BindingCopy(void *op) {
-	void *np = gcnew(Binding);
-	memcpy(np, op, sizeof (Binding));
-	return np;
+static void *BindingCopy(void *original_ptr)
+{   void *new_ptr = gcnew(Binding);
+    memcpy(new_ptr, original_ptr, sizeof(Binding));
+ 
+    return new_ptr;
 }
 
-static size_t BindingScan(void *p) {
-	Binding *binding = p;
-	binding->name = forward(binding->name);
-	binding->defn = forward(binding->defn);
-	binding->next = forward(binding->next);
-	return sizeof (Binding);
+static size_t BindingScan(void *binding_ptr)
+{   Binding *binding = binding_ptr;
+    binding->name    = forward(binding->name);
+    binding->defn    = forward(binding->defn);
+    binding->next    = forward(binding->next);
+    return sizeof(Binding);
 }
